cb1ac83045030056bba43f22bdad655c2b0ff4ea
krisp <krisp@krisp.com>
2016-08-22 23:43:35 -0400

    Added permissions and gravepop command to drop loot at player's location

--- src/com/krisp/minecraft/gravehelper/Gravehelper.java
+++ src/com/krisp/minecraft/gravehelper/Gravehelper.java
@@ -11,11 +11,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.permissions.Permission;
 import org.bukkit.ChatColor;
 
 public class Gravehelper extends JavaPlugin implements Listener {
 	private Logger log;
 	private org.bukkit.Server server; 
+	
+	public final static Permission GRAVEWARP_PERM = new Permission("gravehelper.command.grave");
+	public final static Permission GRAVELIST_PERM = new Permission("gravehelper.command.graves");
+	public final static Permission GRAVEPOP_PERM = new Permission("gravehelper.command.gravepop");
 	
 	public void onEnable() {
 		this.server = Bukkit.getServer();
@@ -33,6 +38,11 @@ public class Gravehelper extends JavaPlugin implements Listener {
 						
 		if(args[0].equalsIgnoreCase("/graves"))
 		{
+			if(!sender.hasPermission(GRAVELIST_PERM)) {
+				sender.sendMessage(ChatColor.RED + "Sorry, you're not allowed to do that.");
+				return;
+			}
+			
 			if(!Grave.gravestones.isEmpty()) {
 				sender.sendMessage(ChatColor.BLUE + "List of graves:");				
 				((ArrayList<Grave>) Grave.gravestones.clone()).forEach(grave -> {									
@@ -47,6 +57,12 @@ public class Gravehelper extends JavaPlugin implements Listener {
 				sender.sendMessage(ChatColor.RED + "No gravestones found");
 			}
 		} else if(args[0].equalsIgnoreCase("/grave")) {			
+			
+			if(!sender.hasPermission(GRAVEWARP_PERM)) {
+				sender.sendMessage(ChatColor.RED + "Sorry, you're not allowed to do that.");
+				return;
+			}
+			
 			if(!Grave.gravestones.isEmpty()) {
 				((ArrayList<Grave>) Grave.gravestones.clone()).forEach(grave -> {
 					if (grave.getPlayerName() == sender.getName()) {
@@ -57,6 +73,22 @@ public class Gravehelper extends JavaPlugin implements Listener {
 				});
 			} else {
 				sender.sendMessage(ChatColor.RED + "No gravestones found");
+			}			
+		} else if(args[0].equalsIgnoreCase("/gravepop")) {
+			if(!sender.hasPermission(GRAVEPOP_PERM)) {
+				sender.sendMessage(ChatColor.RED + "Sorry, you're not allowed to do that.");
+				return;
+			}
+			if(!Grave.gravestones.isEmpty()) {
+				((ArrayList<Grave>) Grave.gravestones.clone()).forEach(grave -> {
+					if (grave.getPlayerName() == sender.getName()) {
+						grave.dropContents(sender.getLocation());
+						grave.destroy();
+						return;
+					}
+				});			
+			} else {
+				sender.sendMessage(ChatColor.RED + "No gravestones found");			
 			}
 		}
 	}
